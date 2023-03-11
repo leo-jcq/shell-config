307428989d56162d42de67bca52034cdd4b889ec
Léo Jacquet <leojacquet2003@gmail.com>
2023-03-11 18:31:26 +0100

    fix bg template

--- pws/Microsoft.PowerShell_profile.ps1
+++ pws/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 oh-my-posh --init --shell pwsh --config "https://raw.githubusercontent.com/leo-jcq/shell-config/main/oh-my-posh/prompt.omp.json" | Invoke-Expression
-Import-Module -Name Terminal-Icons
+# Import-Module -Name Terminal-Icons
